affd42e0a351c2aa44a9154cc374772f27091f3d
Vizcarreitor <leobardo.vizcarra@potros.itson.edu.mx>
2023-03-28 10:08:37 -0700

    Trabajo realizado en el commit: *Se agregaron los colores establecidos en los guidelines *Se agregó la tipografía establecida en los guidelines *Se corrigió el layout activity_main.xml para que usara LinearLayout y los colores correctos, además de cambiar el id de los elementos para que fueran más fáciles de identificar

--- app/src/main/java/nieblas/julio/dclaro/DeclaracionesObligaActivity.kt
+++ app/src/main/java/nieblas/julio/dclaro/DeclaracionesObligaActivity.kt
@@ -3,7 +3,7 @@ package nieblas.julio.dclaro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class DEclaracionesObligaActivity : AppCompatActivity() {
+class DeclaracionesObligaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_declaraciones_obliga)
