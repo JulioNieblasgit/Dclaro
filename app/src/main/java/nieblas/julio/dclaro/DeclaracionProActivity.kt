eebba6ba3bac12973dab5f119aa5b571295fc31d
JulioNieblas <julionieblasgit@gmail.com>
2023-03-27 21:02:03 -0600

    Julio Nieblas

---
+++ app/src/main/java/nieblas/julio/dclaro/DeclaracionProActivity.kt
@@ -0,0 +1,11 @@
+package nieblas.julio.dclaro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeclaracionProActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_declaracion_pro)
+    }
+}
---
+++ app/src/main/java/nieblas/julio/dclaro/SeleccionFacturaActivity.kt
@@ -0,0 +1,11 @@
+package nieblas.julio.dclaro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeleccionFacturaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seleccion_factura)
+    }
+}
