17e5647e167355bcf1ca7461fb13d6333e9d8d4d
Carlos Antonio Valenzuela Valdez <carlotes450@gmail.com>
2023-03-29 23:29:04 -0700

    Diseño de interfaces restablecer contrasenia y perfil

---
+++ app/src/main/java/nieblas/julio/dclaro/RestablecerContraseniaActivity.kt
@@ -0,0 +1,11 @@
+package nieblas.julio.dclaro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RestablecerContraseniaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restablecer_contrasenia)
+    }
+}
