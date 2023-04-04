59f1f58cbdaa5e208bc34ee5466e41e8c47d780a
Vizcarreitor <leobardo.vizcarra@potros.itson.edu.mx>
2023-04-04 15:58:17 -0700

    Trabajo realizado en el commit: *Se corrigieron strings en strings.xml *Se corrigieron errores en pantallas previas *Se agergó una nueva forma gradient_3.xml en drawable *Se corrigió el nombre de algunas pantallas *Se añadió el archivo spinner_options.xml que contendrá todos los datos para los spinners requeridos

--- app/src/main/java/nieblas/julio/dclaro/RestablecerPassActivity.kt
+++ app/src/main/java/nieblas/julio/dclaro/RestablecerPassActivity.kt
@@ -3,9 +3,9 @@ package nieblas.julio.dclaro
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class RestablecerContraseniaActivity : AppCompatActivity() {
+class RestablecerPassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_restablecer_contrasenia)
+        setContentView(R.layout.activity_restablecer_pass)
     }
 }
