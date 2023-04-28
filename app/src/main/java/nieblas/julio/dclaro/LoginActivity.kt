791bb65847c639afae002fbb63ad994c8efc4076
JulioNieblas <julionieblasgit@gmail.com>
2023-04-28 00:56:13 -0600

    conexion exitosa firebase

--- app/src/main/java/nieblas/julio/dclaro/LoginActivity.kt
+++ app/src/main/java/nieblas/julio/dclaro/LoginActivity.kt
@@ -2,8 +2,12 @@ package nieblas.julio.dclaro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
